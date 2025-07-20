5833cd55a419bf85680584a397bb7025db8f2c64
Kai Sugahara <git@kais.jp>
2025-07-20 10:47:45 +0000

    Add: validate_post_text func.

---
+++ rec_paper_bot/lib/__init__.py
@@ -0,0 +1,23 @@
+from twitter_text import parse_tweet
+
+from .poster import Poster as Poster
+from .summarizer import AbstractSummarizer as AbstractSummarizer
+
+
+def validate_post_text(text: str) -> str:
+    """Validate the text for X post
+
+    Args:
+        text (str): The text
+
+    Returns:
+        str: The validated text
+    """
+
+    if parse_tweet(text).valid:
+        return text
+
+    if text.endswith("..."):
+        return validate_post_text(text[:-4] + "...")
+
+    return validate_post_text(text[:-1] + "...")
