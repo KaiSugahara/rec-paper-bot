cb199859f71b01a67d7b88cef8103e4d1dc4dda3
Kai Sugahara <git@kais.jp>
2025-09-23 04:16:21 +0000

    Update: 10 minute retry -> 1 hour retry

--- rec_paper_bot/dags/posting.py
+++ rec_paper_bot/dags/posting.py
@@ -7,7 +7,7 @@ from rec_paper_bot.tasks import get_summary, log_post_time, post_paper_summary, 
 default_args = {
     "owner": "airflow",
     "retries": 3,
-    "retry_delay": timedelta(minutes=10),
+    "retry_delay": timedelta(hours=1),
 }
 
 
