7c12b2d9ec7f95b1a82f24d3b7a9891687195e34
Kai Sugahara <git@kais.jp>
2025-09-23 05:06:49 +0000

    Update: run post_paper summary one by one

--- rec_paper_bot/dags/posting.py
+++ rec_paper_bot/dags/posting.py
@@ -2,7 +2,13 @@ from datetime import timedelta
 
 from airflow.sdk import dag
 from rec_paper_bot.lib.alert_callback import on_failure_callback, on_success_callback
-from rec_paper_bot.tasks import get_summary, log_post_time, post_paper_summary, sample_summarized_but_not_posted_papers
+from rec_paper_bot.tasks import (
+    get_now,
+    get_summary,
+    log_post_time,
+    post_paper_summary,
+    sample_summarized_but_not_posted_papers,
+)
 
 default_args = {
     "owner": "airflow",
@@ -25,7 +31,12 @@ def generate_dag():
     for lang in langs:
         papers = sample_summarized_but_not_posted_papers(lang=lang)
         summaries = get_summary.partial(lang=lang).expand(paper=papers)
-        post_times = post_paper_summary.expand(paper=papers, summary=summaries)
+        post_ids = post_paper_summary.partial(key="title").expand(paper=papers, summary=summaries)
+        for key in ["objective", "methodology", "finding"]:
+            post_ids = post_paper_summary.partial(key=key).expand(
+                paper=papers, summary=summaries, in_reply_to_post_id=post_ids
+            )
+        post_times = get_now.expand(flag=post_ids)
         log_post_time.partial(lang=lang).expand(paper=papers, post_time=post_times)
 
 
--- rec_paper_bot/tasks/post_paper_summary.py
+++ rec_paper_bot/tasks/post_paper_summary.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from typing import Literal, Optional
 
 from airflow.sdk import task
 from rec_paper_bot.lib import Poster, validate_post_text
@@ -6,15 +6,22 @@ from rec_paper_bot.schemas import Paper, PaperSummary
 
 
 @task
-def post_paper_summary(paper: Paper, summary: PaperSummary) -> str:
+def post_paper_summary(
+    paper: Paper,
+    summary: PaperSummary,
+    key: Literal["title", "objective", "methodology", "finding"],
+    in_reply_to_post_id: Optional[str] = None,
+) -> str:
     """Post the paper summary
 
     Args:
         paper (Paper): The paper meta
         summary (PaperSummary): The summary
+        key (Literal["title", "objective", "methodology", "finding"]): The key to post
+        in_reply_to_post_id (Optional[str], optional): The post ID to reply to. Defaults to None.
 
     Returns:
-        str: The post time
+        str: The post ID
     """
 
     lang = summary["lang"]
@@ -22,15 +29,15 @@ def post_paper_summary(paper: Paper, summary: PaperSummary) -> str:
     poster = Poster(lang=lang)
 
     # Post Title and URL of the paper
-    text = validate_post_text(paper["title"] + "\n" + paper["url"])
-    post_id = poster.post(text)
+    if key == "title":
+        text = validate_post_text(paper["title"] + "\n" + paper["url"])
+        post_id = poster.post(text)
+        return post_id
 
     # Post Objectives/Methodology/Findings of the paper
-    for key in ["objective", "methodology", "finding"]:
+    if key in ["objective", "methodology", "finding"]:
         text = validate_post_text(f"[{key.upper()}] {summary[key]}")
-        post_id = poster.post(text, in_reply_to_post_id=post_id)
+        post_id = poster.post(text, in_reply_to_post_id=in_reply_to_post_id)
+        return post_id
 
-    # Get the post time
-    post_time = datetime.now().isoformat(timespec="seconds")
-
-    return post_time
+    raise NotImplementedError(f"Unsupported key: {key}")
